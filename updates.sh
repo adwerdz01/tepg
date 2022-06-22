e409924c039e77bea3eb07891465837c5bde89e4
adwerdz01 <103505418+adwerdz01@users.noreply.github.com>
2022-06-22 14:37:36 +0300

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -1,4 +1,16 @@
-Güncelleme var...
-Çalışmayacaktır. lütfen panel açılınca menuden cancel ile çıkınız
+1) Cpu için Özel optimize edildi.
 
-enter basarak geçebilirsiniz...
+daha esnek ve az cpu kullanımı sağlandı (değişkenlik gösterebilir)
+
+1 - 2 adet auth hatası giderildi.
+
+2) 2 Günlük herkese F5 mod Hediye
+
+Gerekli Kurulum Talimatları:
+
+--------
+https://raw.githubusercontent.com/adwerdz01/tepg/main/mod_kurulumu.txt
+--------
+mouse ile seçin ve Ctrl c ile kopyalayabilirsiniz
+
+enter basarak bu ekranı geçebilirsiniz...
